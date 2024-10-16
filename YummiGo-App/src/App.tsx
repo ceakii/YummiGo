fe919127c5d3e62e0809db35121673d456e38852
Phoebe Jin <phoebejin02@gmail.com>
2024-10-16 15:43:59 -0500

    sf

--- YummiGo-App/src/App.tsx
+++ YummiGo-App/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from 'react';
 
 // Components
 import MuiBottomNavigation from "./components/MuiBottomNavigation";
@@ -11,24 +12,29 @@ import Profile from "./pages/Profile";
 import Quest from "./pages/Quest";
 import Settings from "./pages/Settings";
 
-export default function App() {
+const App = () => {
+  // Set the document title when the component loads
+  useEffect(() => {
+    document.title = 'YummiGo';
+  }, []);
+
   return (
     <>
       <Routes>
-          <Route path="/" element={<Adventure />} />
-          <Route path="/recipe" element={<Recipe />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/quest" element={<Quest />} />
-          <Route path="/settings" element={<Settings />} />
+        <Route path="/" element={<Adventure />} />
+        <Route path="/recipe" element={<Recipe />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/quest" element={<Quest />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
 
-      <h1>
-        Yummigo
-      </h1>
+      <h1>YummiGo</h1>
 
       <div id="nav">
         <MuiBottomNavigation />
       </div>
     </>
-  )
-}
+  );
+};
+
+export default App;
