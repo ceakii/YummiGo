e676dac6482360aee82e3a983e66000cd2985134
Phoebe Jin <phoebejin02@gmail.com>
2024-10-21 15:26:09 -0500

    adding the granola bar

--- YummiGo-App/src/components/RecipeGrid.tsx
+++ YummiGo-App/src/components/RecipeGrid.tsx
@@ -7,6 +7,7 @@ import CardActionArea from '@mui/material/CardActionArea';
 
 // Image Paths
 import SpringRoll from "/images/SpringRoll.jpg"
+import GranolaBars from "/images/GranolaBars.png"
 
 // TODO:
 // - Add more recipes + Descriptions
@@ -14,9 +15,11 @@ import SpringRoll from "/images/SpringRoll.jpg"
 // - Figure out how to not hardcode the maxHeight and make it so that
 //   content stays within the Box via the Style.tsx
 
+
+
 export default function RecipeGrid() {
   return (
-    <Grid container spacing={1} columns={2} maxHeight={870} overflow="auto">
+    <Grid container spacing={1} columns={2} maxHeight={580} overflow="auto">
       <Grid
         display="flex"
         justifyContent="center"
@@ -46,6 +49,69 @@ export default function RecipeGrid() {
         </Card>
       </Grid>
       
+      {/*Second one*/}
+      <Grid
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        size={"auto"}
+      >
+        <Card sx={{ width: "48vw" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height=""
+              image={GranolaBars}
+              alt="Granola Bars"
+              //sx={{ width: '50vw', height: '29vw' }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Granola Bars
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              A granola bar is a versatile baked snack. This homemade granola  bar recipe
+              is fiber and protein packed and calls for a delicious mixture of rolled oats, shredded coconut, 
+              and peanut butter. It's sweetened with honey and vanilla extract, with a pinch of 
+              salt that enhances the other flavors.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+
+
+      {/*Third one*/}
+      <Grid
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        size={"auto"}
+      >
+        <Card sx={{ width: "48vw"}}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height=""
+              image={SpringRoll}
+              alt="Spring Roll"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Spring Roll
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                Vietnamese spring rolls, or "gỏi cuốn," are fresh rice paper rolls 
+                filled with vegetables, herbs, and proteins like shrimp or tofu. 
+                Served with dipping sauces like peanut sauce, they offer vibrant 
+                flavors and a healthy, satisfying bite.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+
+    {/*Fourth one*/}
       <Grid
         display="flex"
         justifyContent="center"
@@ -75,6 +141,7 @@ export default function RecipeGrid() {
         </Card>
       </Grid>
 
+      {/*Fifth one*/}
       <Grid
         display="flex"
         justifyContent="center"
@@ -104,64 +171,8 @@ export default function RecipeGrid() {
         </Card>
       </Grid>
 
-      <Grid
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        size={"auto"}
-      >
-        <Card sx={{ width: "48vw" }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height=""
-              image={SpringRoll}
-              alt="Spring Roll"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Spring Roll
-              </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                Vietnamese spring rolls, or "gỏi cuốn," are fresh rice paper rolls 
-                filled with vegetables, herbs, and proteins like shrimp or tofu. 
-                Served with dipping sauces like peanut sauce, they offer vibrant 
-                flavors and a healthy, satisfying bite.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid>
 
-      <Grid
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        size={"auto"}
-      >
-        <Card sx={{ width: "48vw" }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height=""
-              image={SpringRoll}
-              alt="Spring Roll"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Spring Roll
-              </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                Vietnamese spring rolls, or "gỏi cuốn," are fresh rice paper rolls 
-                filled with vegetables, herbs, and proteins like shrimp or tofu. 
-                Served with dipping sauces like peanut sauce, they offer vibrant 
-                flavors and a healthy, satisfying bite.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid>
-
+      {/*6th one*/}
       <Grid
         display="flex"
         justifyContent="center"
