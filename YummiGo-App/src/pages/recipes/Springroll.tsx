acb904cc04f340aff3009c0b08b6eb9c53db5ffe
kamikitsko <125432520+kamikitsko@users.noreply.github.com>
2024-10-21 21:48:15 -0500

    Springroll to SpringRoll

--- YummiGo-App/src/pages/recipes/Springroll.tsx
+++ YummiGo-App/src/pages/recipes/Springroll.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { pageStyle } from "../../Style";
 
-export default function Springroll() {
+export default function SpringRoll() {
   return (
     <Box sx={pageStyle}>
       Springroll page
