534d300cd9ece5a299bf4afb917a2138e1221982
TheDavisWIN <davishuynh1148@gmail.com>
2024-10-23 19:37:35 -0500

    Updated recipes

--- YummiGo-App/src/pages/recipes/AvocadoSmoothie.tsx
+++ YummiGo-App/src/pages/recipes/AvocadoSmoothie.tsx
@@ -1,10 +1,38 @@
-import { Box } from "@mui/material";
-import { pageStyle } from "../../Style";
+import RecipeContainer from "../../components/RecipeContainer"
+import AvocadoSmoothieImage from "/images/AvocadoSmoothie.png";
 
 export default function AvocadoSmoothie() {
   return (
-    <Box sx={pageStyle}>
-      AvocadoSmoothie page
-    </Box>
+    <RecipeContainer
+      title="Avocado Smoothie"
+      imageSrc={AvocadoSmoothieImage}
+    >
+      {`
+        Prep Time: 5 minutes
+        Total Time: 10 minutes
+        Servings: 1
+
+
+        
+        
+        Ingredients
+
+        • 1 ripe Haas avocado
+        • 1 cup of ice
+        • 1/2 cup whole or coconut milk
+        • 1/4 cup condensed milk
+
+        
+        
+        Directions
+
+        1. Remove flesh of avocado and add to a blender, along with ice cubes, milk, and condensed milk. Blend until totally smooth.
+
+        2. Transfer to a glass and serve immediately with a straw.
+
+      `}
+    </RecipeContainer>
   );
 }
+
+
