8e499c17e3d7da59b4dd52906f2c3702c9f5a800
kamikitsko <125432520+kamikitsko@users.noreply.github.com>
2024-11-08 04:50:55 -0600

    crown functionalities added

--- YummiGo-App/src/pages/avatarcustom/avatarCustom.tsx
+++ YummiGo-App/src/pages/avatarcustom/avatarCustom.tsx
@@ -1,7 +1,17 @@
-import { Box, IconButton, Stack, Popper } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Stack,
+  Popper,
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  ThemeProvider,
+} from "@mui/material";
 import { useState, MouseEvent } from "react";
 
-import { pageStyle } from "../../Style";
+import { pageStyle, textTheme } from "../../Style";
 import avatarimg from "/images/avatar_customization_screen.png";
 import hatIcon from "/images/hat_icon.png";
 import shirtIcon from "/images/ShirtIcon.png";
@@ -12,9 +22,17 @@ import crownIcon from "/images/Crown.png";
 
 export default function AvatarCustom() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorCrown, setAnchorCrown] = useState<null | HTMLElement>(null);
+  const [anchorWiz, setAnchorWiz] = useState<null | HTMLElement>(null);
 
   const handleClick = (event: MouseEvent<HTMLElement>) => {
     setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+  const handleClick2 = (event: MouseEvent<HTMLElement>) => {
+    setAnchorCrown(anchorCrown ? null : event.currentTarget);
+  };
+  const handleClick3 = (event: MouseEvent<HTMLElement>) => {
+    setAnchorWiz(anchorWiz ? null : event.currentTarget);
   };
 
   const open = Boolean(anchorEl);
@@ -51,6 +69,63 @@ export default function AvatarCustom() {
         alt="Avatar customization screen"
         src={avatarimg}
       />
+      {anchorCrown && (
+        <Box
+          sx={{
+            position: "absolute",
+            top: "30%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: (theme) => theme.zIndex.modal + 1,
+            backgroundColor: "#FEAF2F",
+            padding: 2,
+            borderRadius: "10px",
+          }}
+        >
+          <Card
+            sx={{
+              bgcolor: "#FEAF2F",
+              boxShadow: "none",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "30vw",
+              height: "30vh",
+            }}
+          >
+            <CardContent>
+              <ThemeProvider theme={textTheme}>
+                <Typography variant="h4">Wear</Typography>
+              </ThemeProvider>
+              <Box
+                sx={{
+                  borderRadius: "50px",
+                  width: "4vw",
+                  height: "4vw",
+                  bgcolor: "#FFFFFFB0",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  component="img"
+                  src={crownIcon}
+                  alt="Crown icon"
+                  sx={{ ...iconImageStyle }}
+                />
+              </Box>
+
+              <Button onClick={handleClick2}>
+                <ThemeProvider theme={textTheme}>
+                  <Typography variant="h6">Get Crown</Typography>
+                </ThemeProvider>
+              </Button>
+            </CardContent>
+          </Card>
+        </Box>
+      )}
 
       <Stack
         spacing={{ xs: 1, sm: 2 }}
@@ -78,18 +153,23 @@ export default function AvatarCustom() {
             placement="left-end"
             sx={{ pr: "1vw" }}
           >
-            <IconButton sx={{ ...iconButtonStyle }}>
+            <IconButton
+              sx={{ ...iconButtonStyle, marginRight: "0.5vw" }}
+              onClick={handleClick2}
+            >
               <Box
                 component="img"
                 src={crownIcon}
                 alt="Crown icon"
                 sx={{ ...iconImageStyle }}
               />
+            </IconButton>
+            <IconButton sx={{ ...iconButtonStyle }} onClick={handleClick3}>
               <Box
                 component="img"
                 src={wizardHatIcon}
                 alt="wizard hat icon"
-                sx={{ ...iconImageStyle, marginLeft: "1vw" }}
+                sx={{ ...iconImageStyle }}
               />
             </IconButton>
           </Popper>
